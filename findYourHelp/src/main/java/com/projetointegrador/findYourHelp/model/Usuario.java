b08e98c7dcc2fa4cd7b0708934448c6ed87320bc
Carolina Guida <carolinaguidaa@gmail.com>
2021-03-04 16:35:00 -0300

    refactor(model): Add atributes

--- findYourHelp/src/main/java/com/projetointegrador/findYourHelp/model/Usuario.java
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/model/Usuario.java
@@ -1,4 +1,5 @@
 package com.projetointegrador.findYourHelp.model;
+
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -9,76 +10,95 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
 
 @Entity
 @Table(name = "usuario")
 public class Usuario {
-	// create table postagem
-		// criando tabela postagem
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private long idUsuario;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idUsuario;
 
-		@NotNull
-		@Size(min = 8, max = 20)
-		private String senhaUsuario;
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nomeUsuario;
 
-		@NotNull
-		@Email
-		@Size(min = 5, max = 100)
-		private String emailUsuario;
+	@NotNull
+	@Email
+	@Size(min = 5, max = 100)
+	private String emailUsuario;
 
-		
-		@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-		@JsonIgnoreProperties("usuario")
-		private List<Postagem> postagem;
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String senhaUsuario;
 
+	private String imagemUsuario;
 
-		public long getIdUsuario() {
-			return idUsuario;
-		}
+	@NotNull
+	private String tipoUsuario;
 
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
-		public void setIdUsuario(long idUsuario) {
-			this.idUsuario = idUsuario;
-		}
+	public long getIdUsuario() {
+		return idUsuario;
+	}
 
+	public void setIdUsuario(long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
 
-		public String getSenhaUsuario() {
-			return senhaUsuario;
-		}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
 
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
 
-		public void setSenhaUsuario(String senhaUsuario) {
-			this.senhaUsuario = senhaUsuario;
-		}
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
 
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
 
-		public String getEmailUsuario() {
-			return emailUsuario;
-		}
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
 
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
 
-		public void setEmailUsuario(String emailUsuario) {
-			this.emailUsuario = emailUsuario;
-		}
+	public String getImagemUsuario() {
+		return imagemUsuario;
+	}
 
+	public void setImagemUsuario(String imagemUsuario) {
+		this.imagemUsuario = imagemUsuario;
+	}
 
-		public List<Postagem> getPostagem() {
-			return postagem;
-		}
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
 
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
 
-		public void setPostagem(List<Postagem> postagem) {
-			this.postagem = postagem;
-		}
-		
-		
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
 
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 }
