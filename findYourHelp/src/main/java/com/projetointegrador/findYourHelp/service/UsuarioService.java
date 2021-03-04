ece9a466408a7f5477d29fd97577ce37c4f3f996
Carolina Guida <carolinaguidaa@gmail.com>
2021-03-04 17:11:07 -0300

    feat(service): Create package service

---
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/service/UsuarioService.java
@@ -0,0 +1,58 @@
+package com.projetointegrador.findYourHelp.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.projetointegrador.findYourHelp.model.UserLogin;
+import com.projetointegrador.findYourHelp.model.Usuario;
+import com.projetointegrador.findYourHelp.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+
+		if (repository.findByEmailUsuario(usuario.getEmailUsuario()).isPresent()) {
+			return null;
+		}
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String senhaEncoder = encoder.encode(usuario.getSenhaUsuario());
+		usuario.setSenhaUsuario(senhaEncoder);
+
+		return Optional.of(repository.save(usuario));
+	}
+
+	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByEmailUsuario(user.get().getEmailUsuario());
+		if (usuario.isPresent()) {
+			if (encoder.matches(user.get().getSenhaUsuario(), usuario.get().getSenhaUsuario())) {
+
+				String auth = user.get().getNomeUsuario() + ":" + user.get().getSenhaUsuario();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+
+				user.get().setToken(authHeader);
+				user.get().setNomeUsuario(usuario.get().getNomeUsuario());
+				user.get().setSenhaUsuario(usuario.get().getSenhaUsuario());
+				user.get().setTipoUsuario(usuario.get().getTipoUsuario());
+				user.get().setImagemUsuario(usuario.get().getImagemUsuario());
+
+				return user;
+
+			}
+		}
+		return null;
+	}
+
+}
