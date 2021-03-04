f84cd9a0c1da256bb0b4b47fc3968c1c4e262e35
Carolina Guida <carolinaguidaa@gmail.com>
2021-03-04 16:39:40 -0300

    refactor(repository): Add findByEmailUsuario

--- findYourHelp/src/main/java/com/projetointegrador/findYourHelp/repository/UsuarioRepository.java
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.projetointegrador.findYourHelp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.projetointegrador.findYourHelp.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	public Optional<Usuario> findByEmailUsuario(String emailUsuario);
 }
