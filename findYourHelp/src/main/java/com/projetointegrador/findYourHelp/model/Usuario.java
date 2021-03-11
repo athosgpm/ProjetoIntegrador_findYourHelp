16275a8fac617dab87249db54ad5336a33b04674
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2021-03-11 16:18:11 -0300

    refactor(usuario):add Commentary and Validation to Usuario

--- findYourHelp/src/main/java/com/projetointegrador/findYourHelp/model/Usuario.java
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/model/Usuario.java
@@ -1,5 +1,10 @@
+/* Projeto Integrador Find Your Help
+ Comentários criados por: Gustavo de Souza Guilhen e Tatiane Tissoni Antunes. 
+ */
+
 package com.projetointegrador.findYourHelp.model;
 
+// Import de Bibliotecas.
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -10,41 +15,58 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.sun.istack.NotNull;
+import org.hibernate.validator.constraints.URL;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.validation.constraints.NotNull;
+
+// Criação da tabela: Usuario
 @Entity
 @Table(name = "usuario")
 public class Usuario {
 
+// Atributo ID Chave Primaria
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idUsuario;
 
-	@NotNull
-	@Size(min = 2, max = 100)
+// Atributo nomeUsuario com suas caracteristicas	
+	@NotEmpty(message="O nome do usuário é um campo obrigatório.")
+	@NotNull(message="O nome do usuário é um campo obrigatório.")
+	@Size(min = 2, max = 100, message="O nome do usuário deve possuir entre 2 e 100 caracteres.")
 	private String nomeUsuario;
 
-	@NotNull
-	@Email
-	@Size(min = 5, max = 100)
+// Atributo emailUsuario com suas caracteristicas
+	@NotEmpty(message="O E-mail do usuário é um campo obrigatório.")
+	@NotNull(message="O E-mail do usuário é um campo obrigatório.")
+	@Email(message="O E-mail do usuário precisa ser válido.")
+	@Size(min = 5, max = 100, message="O E-mail do usuário deve possuir entre 5 e 100 caracteres.")
 	private String emailUsuario;
 
-	@NotNull
-	@Size(min = 5, max = 100)
+// Atributo senhaUsuario com suas caracteristicas
+	@NotEmpty(message="A senha do usuário é um campo obrigatório.")
+	@NotNull(message="A senha do usuário é um campo obrigatório.")
+	@Size(min = 5, max = 100, message="A senha do usuário deve possuir entre 5 e 100 caracteres.")
 	private String senhaUsuario;
 
+// Atributo imagemUsuario com suas caracteristicas	
+	@URL(message="O campo precisa possuir uma URL.")
 	private String imagemUsuario;
 
-	@NotNull
+// Atributo tipoUsuario com suas caracteristicas
+	@NotEmpty(message="O tipo de usuário é um campo obrigatório.")	
+	@NotNull(message="O tipo de usuário é um campo obrigatório.")
 	private String tipoUsuario;
 
+// Relacionamento entre tabela postagem
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+//Getters e Setters
 	public long getIdUsuario() {
 		return idUsuario;
 	}
