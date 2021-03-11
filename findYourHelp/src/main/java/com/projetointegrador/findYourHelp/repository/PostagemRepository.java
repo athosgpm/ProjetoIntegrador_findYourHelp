ea10c6dee87daa84d8ffa53f1bff536e619315ef
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2021-03-11 17:21:21 -0300

    refactor(userLogin):add Commentary

--- findYourHelp/src/main/java/com/projetointegrador/findYourHelp/repository/PostagemRepository.java
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/repository/PostagemRepository.java
@@ -1,5 +1,10 @@
+/* Projeto Integrador Find Your Help
+ Comentários criados por: Gustavo de Souza Guilhen e Tatiane Tissoni Antunes. 
+ */
+
 package com.projetointegrador.findYourHelp.repository;
 
+// Import de Bibliotecas
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,8 +12,10 @@ import org.springframework.stereotype.Repository;
 
 import com.projetointegrador.findYourHelp.model.Postagem;
 
+// Criação de Repositorio Postagem
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem,Long>
 {
+	// Criação de metodo de procura pelo atributo titulo postagem usado no controller
 		public List<Postagem> findAllByTituloPostagemContainingIgnoreCase(String tituloPostagem); 
 }
