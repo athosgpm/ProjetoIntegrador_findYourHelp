fb83c24e233617879b0af9d22890201c68e1efdf
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2021-03-11 17:25:21 -0300

    refactor(usuarioRepository):add Commentary

--- findYourHelp/src/main/java/com/projetointegrador/findYourHelp/repository/UsuarioRepository.java
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
+/* Projeto Integrador Find Your Help
+ Comentários criados por: Gustavo de Souza Guilhen e Tatiane Tissoni Antunes. 
+ */
+
 package com.projetointegrador.findYourHelp.repository;
 
+// Import de Bibliotecas
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +12,10 @@ import org.springframework.stereotype.Repository;
 
 import com.projetointegrador.findYourHelp.model.Usuario;
 
+// Criação de Repositorio Usuario
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> 
+{
+	// Criação de metodo de procura pelo Atributo EmailUsuario usado no controller
 	public Optional<Usuario> findByEmailUsuario(String emailUsuario);
 }
