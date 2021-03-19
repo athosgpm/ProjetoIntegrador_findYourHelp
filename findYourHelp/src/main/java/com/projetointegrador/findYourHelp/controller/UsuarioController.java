bf03c1dc117e8322082e5b751038008c7db6c9af
MariaNazar <nazare740@gmail.com>
2021-03-19 15:34:05 -0300

    refactor(Comentaries):add comentaries to usuarioController

--- findYourHelp/src/main/java/com/projetointegrador/findYourHelp/controller/UsuarioController.java
+++ findYourHelp/src/main/java/com/projetointegrador/findYourHelp/controller/UsuarioController.java
@@ -1,5 +1,10 @@
+/* Projeto Integrador Find Your Help
+ Comentários criados por: Maria Nazaré Cortez Lima e Andressa Ferreira. 
+ */
+
 package com.projetointegrador.findYourHelp.controller;
 
+//Import de bibliotecas.
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +25,18 @@ import com.projetointegrador.findYourHelp.service.UsuarioService;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
+	//Acha todas as postagens pelo metodo criado no Repository: usuarioRepository.
 	@Autowired
 	private UsuarioService usuarioService;
 
+	//Insere um tema, nesse caso para logar.
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 
+	//Insere um tema, nesse caso para logar.
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
 		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
